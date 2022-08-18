fbffbb89004d805461d689b60d39ad70d88235da
ezeleke <abefikru@gmail.com>
2022-08-18 09:49:56 -0700

    first commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            car mycar =new car();
+            mycar.Make = "Hyundai";
+            mycar.Model = "Santa fe";
+            mycar.Year = 2020;
+
+            Console.WriteLine($"{mycar.Make}");
+            Console.WriteLine($"{mycar.Model}");
+            Console.WriteLine($"{mycar.Year}");
+        }
+    } 
+}
---
+++ ClassesExercise/car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExercise
+{
+    public class car
+    {
+        public car()
+        {
+
+        }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+
+}
